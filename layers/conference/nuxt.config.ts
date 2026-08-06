c194706cfa4002fdbe08cb452d86225ff9dfe167
gifarydoha <gifarydoha@gmail.com>
2026-08-06 19:11:28 +0600

    config updated for build

--- layers/conference/nuxt.config.ts
+++ layers/conference/nuxt.config.ts
@@ -1,4 +1,29 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+const rootDir = join(currentDir, "../..");
+
 export default defineNuxtConfig({
   extends: ["../base"],
+  modules: [
+    "@nuxt/image",
+    "@pinia/nuxt",
+    "@nuxt/icon",
+    "@nuxt/ui",
+    "@vee-validate/nuxt",
+  ],
   srcDir: ".",
+  alias: {
+    "~~": rootDir,
+    "@@": rootDir,
+    "~": rootDir,
+    "@": rootDir,
+    "#layers": join(rootDir, "layers"),
+  },
+  nitro: {
+    prerender: {
+      failOnError: false,
+    },
+  },
 });
